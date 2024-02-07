23917d7e7c88246886fbed8b079b030d4ed9dbf1
九千七 <13733299+zhaoxinyu2004@user.noreply.gitee.com>
2024-02-07 16:37:42 +0800

    生日

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
   //修改密码请改此处
-  if (userName == "111" && pwd == "111") {
+  if (userName == "宣怡婷" && pwd == "20050207") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
